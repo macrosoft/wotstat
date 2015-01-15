1f70ef1cf809bc834789696690f384a4de862ee7
nuciluc <nuciluc@users.noreply.github.com>
2015-01-15 15:47:32 +0100

    vBAddict Battle Rating introduction

--- src/stat.py
+++ src/stat.py
@@ -254,7 +254,9 @@ class SessionStatistic(object):
             'credits': proceeds,
             'gold': value['personal']['gold'] - value['personal']['autoEquipCost'][1] - value['personal']['autoLoadCost'][1],
             'battleTier': battleTier,
-            'assist': value['personal']['damageAssistedRadio'] + value['personal']['damageAssistedTrack']
+            'assist': value['personal']['damageAssistedRadio'] + value['personal']['damageAssistedTrack'],
+            'assistRadio': value['personal']['damageAssistedRadio'],
+            'assistTrack': value['personal']['damageAssistedTrack']
         }
         extended = {
             'vehicle': battle['vehicle'],
@@ -378,7 +380,8 @@ class SessionStatistic(object):
         totalBattleTier = 0
         valuesKeys = ['winsCount', 'defeatsCount', 'drawsCount', 'totalDmg', 'totalFrag', 'totalSpot',\
             'totalDef', 'totalCap', 'totalShots', 'totalHits', 'totalPierced', 'totalAssist',\
-            'totalXP', 'totalOriginXP', 'totalFreeXP', 'credits', 'gold']
+            'totalXP', 'totalOriginXP', 'totalFreeXP', 'credits', 'gold',\
+            'totalAssistRadio', 'totalAssistTrack']
         for key in valuesKeys:
             values[key] = 0
         expKeys = ['expDamage', 'expFrag', 'expSpot', 'expDef', 'expWinRate']
@@ -397,6 +400,8 @@ class SessionStatistic(object):
             values['totalHits'] += battle['hits']
             values['totalPierced'] += battle['pierced']
             values['totalAssist'] += battle['assist']
+            values['totalAssistRadio'] += battle['assistRadio']
+            values['totalAssistTrack'] += battle['assistTrack']
             values['totalXP'] += battle['xp']
             values['totalOriginXP'] += battle['originalXP']
             values['totalFreeXP'] += battle['freeXP']
@@ -424,6 +429,8 @@ class SessionStatistic(object):
             values['avgHitsRate'] = float(values['totalHits'])/max(1, values['totalShots'])*100
             values['avgEffHitsRate'] = float(values['totalPierced'])/max(1, values['totalHits'])*100
             values['avgAssist'] = int(values['totalAssist'])/values['battlesCount']
+            values['avgAssistRadio'] = int(values['totalAssistRadio'])/values['battlesCount']
+            values['avgAssistTrack'] = int(values['totalAssistTrack'])/values['battlesCount']
             values['avgXP'] = int(values['totalXP']/values['battlesCount'])
             values['avgOriginalXP'] = int(values['totalOriginXP']/values['battlesCount'])
             values['avgPremXP'] = int(1.5*values['avgOriginalXP'])
@@ -453,10 +460,15 @@ class SessionStatistic(object):
                 else int(max(min(values['EFF']*(values['EFF']*(values['EFF']*(values['EFF']*\
                 (values['EFF']*(0.00000000000000003388*values['EFF'] - 0.0000000000002469) + \
                 0.00000000069335) - 0.00000095342) + 0.0006656) -0.1485) - 0.85, 100), 0))
+            values['vEFF'] = max(0, int(values['avgDamage']*(1.5/(values['avgTier'] + 0.2)) + \
+                values['avgFrag'] * (350 - values['avgTier'] * 20) + \
+                ((values['avgAssistRadio']/2)*(1.5/(values['avgTier'] + 0.2)) + \
+                ((values['avgAssistTrack']/2)*(1.5/(values['avgTier'] + 0.2)) + \
+                values['avgSpot'] * 200 + values['avgCap'] * 150 + values['avgDef'] * 150 ))
         else:
             for key in ['avgWinRate', 'avgDamage', 'avgFrag', 'avgSpot', 'avgDef', 'avgCap', 'avgHitsRate', \
                 'avgEffHitsRate', 'avgAssist', 'avgXP', 'avgOriginalXP', 'avgPremXP', 'avgCredits', 'avgTier', \
-                'avgBattleTier', 'medPlace', 'WN6', 'XWN6', 'EFF', 'XEFF']:
+                'avgBattleTier', 'medPlace', 'WN6', 'XWN6', 'EFF', 'XEFF', 'vEFF']:
                 values[key] = 0
             for key in expKeys:
                 values[key] = 1
